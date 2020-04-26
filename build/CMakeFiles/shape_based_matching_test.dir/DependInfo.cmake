59a7cbbc159569cc1df525d04cd4a0ea6d0ae41c
yaoyaolii <swingsvm@gmail.com>
2020-04-26 10:58:53 +0800

    first commit

---
+++ build/CMakeFiles/shape_based_matching_test.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/liyao/Project/6D-Pose/shape_based_matching-master/line2Dup.cpp" "/Users/liyao/Project/6D-Pose/shape_based_matching-master/build/CMakeFiles/shape_based_matching_test.dir/line2Dup.cpp.o"
+  "/Users/liyao/Project/6D-Pose/shape_based_matching-master/test.cpp" "/Users/liyao/Project/6D-Pose/shape_based_matching-master/build/CMakeFiles/shape_based_matching_test.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../MIPP"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/shape_based_matching_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shape_based_matching_test.dir/line2Dup.cpp.o"
+  "CMakeFiles/shape_based_matching_test.dir/test.cpp.o"
+  "shape_based_matching_test"
+  "shape_based_matching_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shape_based_matching_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
